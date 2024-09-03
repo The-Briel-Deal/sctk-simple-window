5d37c6987e6687c3d2912a41c965a96f69c2dae7
Gabriel Ford <gabe@fordltc.net>
2024-09-02 20:40:07 -0400

    Add draw method and rand crate.

--- src/state/window.rs
+++ src/state/window.rs
@@ -1,3 +1,5 @@
+use std::i32;
+
 use smithay_client_toolkit::{
     delegate_xdg_shell, delegate_xdg_window,
     shell::{xdg::window::WindowHandler, WaylandSurface},
@@ -24,26 +26,7 @@ impl WindowHandler for GfState {
 
         if self.first_configure {
             self.first_configure = false;
-            // Draw
-            {
-                let width = self.width;
-                let height = self.height;
-                self.buffer = match self.slot_pool().create_buffer(
-                    width as i32,
-                    height as i32,
-                    width as i32 * 4,
-                    wl_shm::Format::Argb8888,
-                ) {
-                    Ok(buffer) => Some(buffer.0),
-                    Err(err) => panic!("Failed to create buffer.\nErr: {err}"),
-                };
-                let buffer = self.buffer.as_ref().expect("Set to Some above.");
-                // Handle return!
-                buffer
-                    .attach_to(self.window().wl_surface())
-                    .expect("Attaching Buffer failed.");
-                self.window().commit();
-            }
+            self.draw();
         }
     }
     fn request_close(
@@ -52,5 +35,35 @@ impl WindowHandler for GfState {
         _qh: &wayland_client::QueueHandle<Self>,
         _window: &smithay_client_toolkit::shell::xdg::window::Window,
     ) {
+    }
+}
+
+impl GfState {
+    fn draw(&mut self) {
+        let width = self.width as i32;
+        let height = self.height as i32;
+        let stride = width * 4;
+        self.buffer =
+            match self
+                .slot_pool()
+                .create_buffer(width, height, stride, wl_shm::Format::Argb8888)
+            {
+                Ok(buffer) => Some(buffer.0),
+                Err(err) => panic!("Failed to create buffer.\nErr: {err}"),
+            };
+        let pool = self.pool.as_mut().unwrap();
+        let buffer = self.buffer.as_ref().expect("Set to Some above.");
+        // TODO: Create a fallback for this.
+        let canvas = pool.canvas(buffer).expect("Getting Canvas Failed.");
+        canvas.chunks_exact_mut(4).for_each(|chunk| {
+            chunk[0] = 0xFF;
+            chunk[1] = rand::random();
+            chunk[2] = 0xFF;
+            chunk[3] = 0xFF;
+        });
+        buffer
+            .attach_to(self.window().wl_surface())
+            .expect("Attaching Buffer failed.");
+        self.window().commit();
     }
 }
